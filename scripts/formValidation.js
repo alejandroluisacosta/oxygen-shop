99f6373dd7944eebe13a5b5b52259232ed2fcace
Alejandro Acosta <alejandroluisacosta95@gmail.com>
2024-04-23 17:45:05 +0200

    add: logic improvements in form validation

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -6,16 +6,40 @@ document.addEventListener("DOMContentLoaded", function() {
         const userName = nameInput.value;
         const emailInput = document.getElementById("email");
         const userEmail = emailInput.value;
-        const form = document.getElementById("policies-check");
+        const checkbox = document.getElementById("policies-check");
         const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
         event.preventDefault();
 
-        if (userName.length < 2 || userName.length > 100)
-            nameInput.style.borderBottom = "1.5px solid red";
+        const checkValues = () => {
+            if (userName.length < 2 || userName.length > 100) {
+                nameInput.style.cssText = "border-bottom: 1.5px solid red";
+                alert("Enter a valid name (Between 2 and 100 characters)");
+                return false;
+            }
+            else {
+                nameInput.style.cssText = "";
+            }
+            if (!emailRegex.test(userEmail)) {
+                emailInput.style.cssText = "border-bottom: 1.5px solid red";
+                alert("Enter a valid email");
+                return false;
+            }
+            else {
+                emailInput.style.cssText = "";
+            }
+            if (!checkbox.checked) {
+                checkbox.style.cssText = "outline: 1.5px solid red;";
+                alert("You need to agree to the data protection policies")
+                return false;
+            }
+            else {
+                checkbox.style.cssText = "";
+            }
+            return true;
+        }
+
+        if (checkValues())
         
-        
-        if (!emailRegex.test(userEmail))
-            emailInput.style.borderBottom = "1.5px solid red";
     })
 })
