87f2a5ccf97289312bf30648a811ddf395c06534
Alejandro Acosta <alejandroluisacosta95@gmail.com>
2024-04-23 16:08:01 +0200

    add: percentage scroll bar

--- scripts/percentageScroller.js
+++ scripts/percentageScroller.js
@@ -1,11 +1,10 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    const body = document.querySelector(body);
-    const percentageScroller = document.getElementById("nav__percentage__scroller");
+    const body = document.querySelector("body");
+    const percentageScroller = document.getElementById("nav__percentage-scroller");
 
-    scroller.addEventListener("scroll", () => {
-        let scrolledPercentage = (body.scrollTop / body.scrollHeight) * 100;
-        percentageScroller.style.backgroundColor = "black";
-        percentageScroller.style.width = scrolledPercentage;
+    window.addEventListener("scroll", () => {
+        let scrolledPercentage = (document.documentElement.scrollTop / (body.scrollHeight - window.innerHeight)) * 100;
+        percentageScroller.style.width = scrolledPercentage + "%";
     });
 })
