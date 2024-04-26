a53c0e793b2225d816750e2661eb3252ed762a2b
Alejandro Acosta <alejandroluisacosta95@gmail.com>
2024-04-26 15:14:04 +0200

    fix: nav event applied to icons instead of navbar

--- scripts/navMenu.js
+++ scripts/navMenu.js
@@ -1,37 +1,36 @@
 
-document.addEventListener("DOMContentLoaded", function() {
 
-    const hamburgerMenu = document.getElementById("nav");
-    const header = document.getElementById("header");
-    const logo = document.getElementsByClassName("nav__content-container__logo");
-    const listPage = document.getElementsByClassName("nav__content-container__list__page");
-    const hamburgerIcon = document.getElementsByClassName("nav__content-container__menu");
-    const closeIcon = document.getElementsByClassName("nav__content-container__close");
+const hamburgerMenu = document.getElementById("nav");
+const header = document.getElementById("header");
+const logo = document.getElementsByClassName("nav__content-container__logo");
+const listPage = document.getElementsByClassName("nav__content-container__list__page");
+const hamburgerIcon = document.getElementsByClassName("nav__content-container__menu");
+const closeIcon = document.getElementsByClassName("nav__content-container__close");
 
-    hamburgerMenu.addEventListener("click", () => {
-            if (window.innerWidth < 1000) {
-                if (hamburgerMenu.classList.contains("closed")) {
-                    hamburgerMenu.style.cssText = "max-height: 14.688rem; z-index: 1; padding-bottom: 1.188rem; background-color: white;";
-                    header.style.cssText = "padding-top: 10.625rem";
-                    logo[0].style.cssText = "display: block; margin-bottom: 2.875rem";
-                    Array.from(listPage).forEach(element => {
-                        element.style.cssText = "display: block";
-                    })
-                    hamburgerIcon[0].style.cssText = "display: none; visibility: hidden; opacity: 0";
-                    closeIcon[0].style.cssText = "visibility: visible; opacity: 1; position: absolute; right: 1.875rem; top: 1.5rem; color: var(--oxy-light-blue)";
-                    hamburgerMenu.classList.toggle("closed");
-                }
-                else {
-                    hamburgerMenu.style.cssText = "transition: max-height 300ms linear; max-height: 4.375rem;";
-                    header.style.cssText = "";
-                    logo[0].style.cssText = "";
-                    Array.from(listPage).forEach(element => {
-                        element.style.cssText = "";
-                    })
-                    hamburgerIcon[0].style.cssText = "";
-                    closeIcon[0].style.cssText = "";
-                    hamburgerMenu.classList.toggle("closed");
-                }
-            }
+hamburgerIcon[0].addEventListener("click", () => {
+    if (window.innerWidth < 1000) {
+        hamburgerMenu.style.cssText = "max-height: 14.688rem; z-index: 1; padding-bottom: 1.188rem; background-color: white;";
+        header.style.cssText = "padding-top: 10.625rem";
+        logo[0].style.cssText = "display: block; margin-bottom: 2.875rem";
+        Array.from(listPage).forEach(element => {
+            element.style.cssText = "display: block";
         })
+        hamburgerIcon[0].style.cssText = "display: none; visibility: hidden; opacity: 0";
+        closeIcon[0].style.cssText = "visibility: visible; opacity: 1; position: absolute; right: 1.875rem; top: 1.5rem; color: var(--oxy-light-blue)";
+        hamburgerMenu.classList.toggle("closed");
+    }
+})
+
+closeIcon[0].addEventListener("click", () => {
+    if (window.innerWidth < 1000) {
+        hamburgerMenu.style.cssText = "";
+        header.style.cssText = "";
+        logo[0].style.cssText = "";
+        Array.from(listPage).forEach(element => {
+            element.style.cssText = "";
+        })
+        hamburgerIcon[0].style.cssText = "";
+        closeIcon[0].style.cssText = "";
+        hamburgerMenu.classList.toggle("closed");
+    }
 })
