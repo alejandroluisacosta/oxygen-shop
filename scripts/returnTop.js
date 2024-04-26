5b9dff1e05a59359dc8c05596342b0620d93d07e
Alejandro Acosta <alejandroluisacosta95@gmail.com>
2024-04-26 14:47:25 +0200

    fix: return to top appears after 25% scrolled

--- scripts/returnTop.js
+++ scripts/returnTop.js
@@ -1,19 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    
-    const returnTopBtn = document.getElementById('return-top');
 
-    returnTopBtn.addEventListener("click", () => {
+const returnTopBtn = document.getElementById('return-top');
+const body = document.querySelector("body");
+
+window.addEventListener("scroll", () => {
+    if (document.documentElement.scrollTop > (body.scrollHeight - window.innerHeight) * 0.25)
+        returnTopBtn.classList.add("shown");
+    else
+        returnTopBtn.classList.remove("shown");
+})
+
+returnTopBtn.addEventListener("click", () => {
+    setTimeout(() => {
+        window.scrollTo(top);
+        returnTopBtn.style.backgroundColor = "#004BA8";
+        returnTopBtn.style.border = "2px solid black";
+        returnTopBtn.style.boxShadow = "inset 2px 2px 4px rgba(0, 0, 0, 0.2), 2px 2px 4px rgba(0, 0, 0, 0.2)";
         setTimeout(() => {
-            window.scrollTo(top);
-            returnTopBtn.style.backgroundColor = "#004BA8";
-            returnTopBtn.style.border = "2px solid black";
-            returnTopBtn.style.boxShadow = "inset 2px 2px 4px rgba(0, 0, 0, 0.2), 2px 2px 4px rgba(0, 0, 0, 0.2)";
-            setTimeout(() => {
-                returnTopBtn.style.backgroundColor = "var(--oxy-blue)";
-                returnTopBtn.style.border = "0";
-                returnTopBtn.style.boxShadow = "none";
-            }, 100) 
-        }, 200);
-    })
+            returnTopBtn.style.backgroundColor = "var(--oxy-blue)";
+            returnTopBtn.style.border = "0";
+            returnTopBtn.style.boxShadow = "none";
+        }, 100) 
+    }, 200);
 })
 
