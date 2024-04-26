c74ede7cb1641ad1c72a3d1109771cea3bdb60ae
Alejandro Acosta <alejandroluisacosta95@gmail.com>
2024-04-26 13:00:19 +0200

    fix: form validation code structure

--- scripts/formValidation.js
+++ scripts/formValidation.js
@@ -1,3 +1,27 @@
+export const checkValues = (nameInput, userName, emailInput, userEmail, checkbox) => {
+    console.log(userName.length);
+    if (userName.length < 2 || userName.length > 100) {
+        nameInput.style.cssText = "border-bottom: 1.5px solid red"; // Dar estilos con una clase aparte
+        alert("Enter a valid name (Between 2 and 100 characters)");
+        return false; // Mostrar todos los errores en un solo alert
+    }
+
+    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    if (!emailRegex.test(userEmail)) {
+        emailInput.style.cssText = "border-bottom: 1.5px solid red";
+        alert("Enter a valid email");
+        return false;
+    }
+
+    if (!checkbox.checked) {
+        checkbox.style.cssText = "outline: 1.5px solid red;";
+        alert("You need to agree to Oxygen Shop's terms and conditions")
+        return false;
+    }
+    
+    return true;
+}
+
 export const submitForm = (userName, userEmail) => {
     fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'POST',
@@ -9,61 +33,33 @@ export const submitForm = (userName, userEmail) => {
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
         },
-    })
-    .then((response) => response.json())
-    .then((json) => { 
-        console.log(json); 
-        alert("Form submitted successfully")
-    }) 
-    .catch((error) => console.error('Error:', error));
+        })
+        .then((response) => response.json())
+        .then((json) => { 
+            console.log(json); 
+            alert("Form submitted successfully")
+        }) 
+        .catch((error) => console.error('Error:', error));
 }
-
 
 document.querySelector("form").addEventListener("submit", (event) => {
     const nameInput = document.getElementById("name");
-    const emailInput = document.getElementById("email");
-    const checkbox = document.getElementById("policies-check");
     const userName = nameInput.value;
+    console.log(userName);
+    const emailInput = document.getElementById("email");
     const userEmail = emailInput.value;
-    const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    
-    event.preventDefault();
-    
-    const checkValues = () => {
-        if (userName.length < 2 || userName.length > 100) {
-            console.log(userName);
-            nameInput.style.cssText = "border-bottom: 1.5px solid red"; // Dar estilos con una clase aparte
-            alert("Enter a valid name (Between 2 and 100 characters)");
-            return false; // Mostrar todos los errores en un solo alert
-        }
-        else {
-            nameInput.style.cssText = "";
-        }
-        if (!emailRegex.test(userEmail)) {
-            console.log(userEmail);
-            emailInput.style.cssText = "border-bottom: 1.5px solid red";
-            alert("Enter a valid email");
-            return false;
-        }
-        else {
-            emailInput.style.cssText = "";
-        }
-        if (!checkbox.checked) {
-            checkbox.style.cssText = "outline: 1.5px solid red;";
-            alert("You need to agree to the data protection policies")
-            return false;
-        }
-        else {
-            checkbox.style.cssText = "";
-        }
+    const checkbox = document.getElementById("policies-check");
 
+    event.preventDefault();
+
+    const checked = checkValues(nameInput, userName, emailInput, userEmail, checkbox);
+    if (checked) {
+        submitForm(userName, userEmail);
+        nameInput.style.cssText = "";
+        emailInput.style.cssText = "";
+        checkbox.style.cssText = "";
         nameInput.value = "";
         emailInput.value = "";
         checkbox.checked = false;
-        
-        return true;
     }
-
-    if (checkValues())
-        submitForm(userName, userEmail); 
 })
--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -1,37 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const newsletterPopUp = document.getElementById("pop-up-modal");
-    const newsletterCloseBtn = document.getElementById("pop-up-modal__close");
+import { submitForm } from "./formValidation.js";
 
-    setTimeout(() => {
-        if (localStorage.getItem("newsletterShown") === "false") {
-            newsletterPopUp.style.display = "flex";
-        }
-    }, 5000);
+const newsletterPopUp = document.getElementById("pop-up-modal");
+const newsletterCloseBtn = document.getElementById("pop-up-modal__close");
 
-    window.addEventListener("scroll", () => {
-        if (window.scrollY / document.documentElement.scrollHeight >= 0.25 && localStorage.getItem("newsletterShown") === "false" ) {
-            newsletterPopUp.style.display = "flex";
-        }
-    })
+setTimeout(() => {
+    if (localStorage.getItem("newsletterShown") === "false") {
+        newsletterPopUp.style.display = "flex";
+    }
+}, 5000);
 
-    newsletterCloseBtn.addEventListener("click", () => {
+window.addEventListener("scroll", () => {
+    if (window.scrollY / document.documentElement.scrollHeight >= 0.25 && localStorage.getItem("newsletterShown") === "false" ) {
+        newsletterPopUp.style.display = "flex";
+    }
+})
+
+newsletterCloseBtn.addEventListener("click", () => {
+    newsletterPopUp.style.display = "none";
+    localStorage.setItem("newsletterShown", "true");
+})
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
         newsletterPopUp.style.display = "none";
         localStorage.setItem("newsletterShown", "true");
-    })
 
-    document.addEventListener("keydown", (event) => {
-        if (event.key === "Escape") {
-            newsletterPopUp.style.display = "none";
-            localStorage.setItem("newsletterShown", "true");
+    }
+})
 
-        }
-    })
+document.addEventListener("click", (event) => {
+    if (event.target !== newsletterPopUp) {
+        newsletterPopUp.style.display = "none";
+        localStorage.setItem("newsletterShown", "true");
 
-    document.addEventListener("click", (event) => {
-        if (event.target !== newsletterPopUp) {
-            newsletterPopUp.style.display = "none";
-            localStorage.setItem("newsletterShown", "true");
+    }
+})
 
-        }
-    })
+newsletterPopUp.addEventListener("submit", () => {
+
 })
