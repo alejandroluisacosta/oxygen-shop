d0b54a103b09a2cc1d5ca3ca5386d8f10223fb52
Alejandro Acosta <alejandroluisacosta95@gmail.com>
2024-04-28 08:56:40 +0200

    fix: newsletter closes when clicked outside of it

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -3,48 +3,47 @@ import { submitForm, checkValues } from "./formValidation.js";
 const newsletterPopUp = document.getElementById("pop-up-modal");
 const newsletterCloseBtn = document.getElementById("pop-up-modal__close");
 const newsletterSubmitBtn = document.getElementById("newsletterSubmitBtn");
-const newsletterTitle = document.getElementById("newsletterTitle");
-const newsletterDescription = document.getElementById("newsletterDescription");
-const newsletterNameInput = document.getElementById("newsletterNameInput");
-const newsletterNameLabel = document.getElementById("newsletterNameLabel");
-const newsletterEmailLabel = document.getElementById("newsletterEmailLabel");
-const newsletterEmailInput = document.getElementById("newsletterEmailInput");
-const newsletterPoliciesCheck = document.getElementById("newsletterPoliciesCheck");
-const newsletterPoliciesText = document.getElementById("newsletterPoliciesText");
-const newsletterButtonContainer = document.getElementById("newsletterButtonContainer");
 
 setTimeout(() => {
     if (localStorage.getItem("newsletterShown") === "false") {
-        newsletterPopUp.style.display = "flex";
+        newsletterPopUp.classList.add("opened");
     }
 }, 5000);
 
 window.addEventListener("scroll", () => {
     if (window.scrollY / document.documentElement.scrollHeight >= 0.25 && localStorage.getItem("newsletterShown") === "false" ) {
-        newsletterPopUp.style.display = "flex";
+        newsletterPopUp.classList.add("opened");
     }
 })
 
 newsletterCloseBtn.addEventListener("click", () => {
-    newsletterPopUp.style.display = "none";
+        newsletterPopUp.classList.remove("opened");
     localStorage.setItem("newsletterShown", "true");
 })
 
 document.addEventListener("keydown", (event) => {
     if (event.key === "Escape") {
-        newsletterPopUp.style.display = "none";
+        newsletterPopUp.classList.remove("opened");
         localStorage.setItem("newsletterShown", "true");
 
     }
 })
 
-// document.addEventListener("click", (event) => {
-//     if (event.target !== newsletterPopUp) {
-//         newsletterPopUp.style.display = "none";
-//         localStorage.setItem("newsletterShown", "true");
-//     }
-// })
+document.addEventListener("click", (event) => {
+    if (event.target !== newsletterPopUp) {
+        newsletterPopUp.classList.remove("opened");
+        localStorage.setItem("newsletterShown", "true");
+    }
+})
 
+const newsletterChildren = Array.from(newsletterPopUp.querySelectorAll("*"));
+newsletterChildren.forEach(element => {
+    if (element.id !== "pop-up-modal__close") {
+        element.addEventListener("click", (event) => {
+            event.stopPropagation();
+        })
+    }
+})
 
 newsletterSubmitBtn.addEventListener("click", (event) => {
     const nameInput = document.getElementById("newsletterNameInput");
@@ -64,5 +63,7 @@ newsletterSubmitBtn.addEventListener("click", (event) => {
         nameInput.value = "";
         emailInput.value = "";
         checkbox.checked = false;
+        newsletterPopUp.classList.remove("opened");
+        localStorage.setItem("newsletterShown", "true");
     }
 })
